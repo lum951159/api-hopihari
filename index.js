c923ae1c4c0e9f828f1473ccf76342e9c9a2e94c
lum951159 <luizmboas@gmail.com>
2025-04-28 15:38:12 -0300

    28.04

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+
+app.use(cors());
+app.use(helmet());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Autohorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    }
+    next();
+});
+
+module.exports = app; 
+
