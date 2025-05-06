a5d95606630980e44840ec4c2cdc2adcea50a63b
luizmboas@gmail.com <lum951159>
2025-05-06 10:38:34 -0300

    sla

--- controllers/usuarios.controllers.js
+++ controllers/usuarios.controllers.js
@@ -1,4 +1,6 @@
 const mysql = require("../mysql");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.atualizarUsuario = async (req, res) => {
     try {
@@ -33,17 +35,54 @@ exports.cadastraUsuario = async (req, res) => {
             `INSERT INTO users (first_name, last_name, email, password, birth_date, phone)
             VALUES (?, ?, ?, ?, ?, ?)`,
             [req.body.first_name, req.body.last_name, req.body.email, req.body.password, req.body.birth_date, req.body.phone],
-            
+
         );
 
         return res.status(201).send({
             "Mensagem": "Usuario criado com sucesso",
             "Resultado": resultado
-             
+
         });
 
-        
+
     } catch (error) {
         return res.status(500).send({ "Mensagem": error.message });
     }
 };
+
+exports.login = async (req, res) => {
+    try {
+        const usuario = await mysql.execute(
+            'SELECT * FROM users WHERE email = ?',
+            [req.body.email]);
+        console.log(usuario);
+
+        if (usuario.length == 0) {
+            return res.status(401).send({ "Mensagem": "Usuario não encontrado" });
+        }
+
+        const hash = await bcrypt.hash(req.body.password, 10);
+        console.log(hash);
+        const match = await bcrypt.compare(req.body.password, usuario[0].password);
+        if (!match) {
+            return res.status(401).send({ "Mensagem": "Senha incorreta" });
+        }
+        console.log(match, req.body.password, usuario[0].password);
+        const token = jwt.sign({
+            id: usuario [0].id,
+            first_name: usuario [0].first_name,
+            last_name: usuario [0].last_name,
+            email: usuario [0].email,
+            birth_date : usuario [0].birth_date,
+            phone: usuario [0].phone
+        }, "senhafojwt");
+        return res.status(200).send({
+            "Mensagem": "Usuario logado com sucesso",
+            "Resultado": usuario,
+            "token": token
+        });
+
+    } catch (error) {
+        return res.status(500).send({ "Error": error });
+    }
+}
--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const usuariosController = require("../controllers/usuarios.controllers");
 
 
+router.post("/login", usuariosController.login);
 router.post("/cadastrar", usuariosController.cadastraUsuario);
 router.put("/:id", usuariosController.atualizarUsuario);
 
