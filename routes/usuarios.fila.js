89598b16a906b74722bd135a9d41847f12ac4044
luizmboas@gmail.com <lum951159>
2025-05-13 08:21:00 -0300

    13.05

--- routes/usuarios.fila.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const login = require("../middleware/usuarios.midware");
-
-router.post("/login", (req, res) => {console.log("Rota da Fila")});
-
-module.exports = router;
